95775ef131312b5888f78d7fae4db98e90c855e5
Oleksandr Borysenko <promzveno@gmail.com>
2023-07-05 08:52:08 +0300

    Create an authorization screen.

--- Ruhanka/ViewController.swift
+++ Ruhanka/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
+    @IBOutlet weak var appleButton: UIButton!
+       
+    
 }
+
+
 
