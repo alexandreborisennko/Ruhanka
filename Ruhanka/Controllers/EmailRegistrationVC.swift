792e903c7882be4502d8468d61777f9209c36ffa
Oleksandr Borysenko <promzveno@gmail.com>
2023-07-06 18:22:35 +0300

    Make function MakeRoundCornersForTextOutlet in separate file

--- Ruhanka/Controllers/EmailRegistrationVC.swift
+++ Ruhanka/Controllers/EmailRegistrationVC.swift
@@ -26,16 +26,7 @@ class EmailRegistrationVC: UIViewController {
         sender.isSelected = !sender.isSelected
     }
     
-    //MARK: -  makeRoundCornersForTextOutlet, Function that makes round corners for Text Inputs
-    
-    func makeRoundCornersForTextOutlet(for textFields: UITextField...) { //... is a variadic parameter
-        
-        for textField in textFields {
-            textField.layer.borderWidth = 1
-            textField.layer.borderColor = #colorLiteral(red: 0.6571641564, green: 0.6571640372, blue: 0.6571640372, alpha: 1)
-            textField.layer.cornerRadius = 10
-            textField.layer.masksToBounds = true
-        }
-    }
+
+  
     
 }
--- Ruhanka/Controllers/LogInVC.swift
+++ Ruhanka/Controllers/LogInVC.swift
@@ -12,7 +12,16 @@ class LogInVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        makeRoundCornersForTextOutlet(for: emailOutlet,passwordOutlet)
 
     }
+
+    @IBOutlet weak var emailOutlet: UITextField!
+    @IBOutlet weak var passwordOutlet: UITextField!
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
     
+
 }
+
